836ec5ed6df43fcd8f699cf02b29349a12ec96f8
dlee-mitre <85962425+dlee-mitre@users.noreply.github.com>
2021-07-06 16:45:57 -0400

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,10 @@
+# 1. Ask the user to enter a number "x"
+
+
+# 2. Ask the user to enter a number "y"
+
+
+# 3. Prints out the number "x" raised to the power "y"
+
+
+# 4. Prints out the log (base 2) of "x"
